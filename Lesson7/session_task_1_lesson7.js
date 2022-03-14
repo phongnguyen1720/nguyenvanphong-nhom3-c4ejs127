1401a6943b7586c0b5d9e015af48bd8121d29982
unknown <phongnguyen.1720@gmail.com>
2022-03-14 09:14:06 +0700

    up bai tap

---
+++ Lesson7/session_task_1_lesson7.js
@@ -0,0 +1,22 @@
+//Bài 1: 1.Viết chương trình tính căn bậc 2 của các số ngẫu nhiên
+// let a = parseInt(prompt("Nhập vào số cần tính phương trình bậc 2"));
+// let canBacHai = Math.sqrt(a);
+// console.log(`Căn bậc 2 của số vừa nhập là: ${canBacHai}`);
+
+//Bài 2: Viết chương trình tính diện tích của một tam giác
+// let a = parseInt(prompt("Nhập vào số a"));
+// let b = parseInt(prompt("Nhập vào số b"));
+// let c = parseInt(prompt("Nhập vào số c"));
+// let chuViTamGiac = a+b+c;
+// console.log(`Chu vi của tam giác là: ${chuViTamGiac}`);
+
+//Bài 3: Viết chương trình chuyển từ độ F sang độ C
+// let a = parseInt(prompt("Nhập nhập giá trị F"));
+// let quyDoiNhietDo = (a - 32) /1.8;
+// console.log(`Độ C cần quy đổi là: ${quyDoiNhietDo}`);
+
+//Bài 4: Viết chương trình chuyển từ km sang m
+// let a = parseInt(prompt("Số Km cần quy đổi là:"));
+// let quyDoiKhoangCach = a*1000;
+// console.log(`Số M cần đổi là: ${quyDoiKhoangCach}`);
+
